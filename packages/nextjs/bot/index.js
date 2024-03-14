5d11a3347d86ea07aee7e3d96d51ae4d769cf52e
Sam Wellander <samwellander@gmail.com>
2024-03-13 21:20:49 -0700

    refactor probot listener
    and add config.yml file when app is installed on a new repo

--- packages/nextjs/bot/index.js
+++ packages/nextjs/bot/index.js
@@ -1,70 +1,20 @@
+const { handleNewRepo } = require("./utils");
+
 module.exports = app => {
   app.on("installation", async context => {
-    for (const addedRepo of context.payload.repositories) {
+    for (const repoData of context.payload.repositories) {
       handleNewRepo({
         context,
-        addedRepo,
+        repoData,
       });
     }
   });
   app.on("installation_repositories", async context => {
-    for (const addedRepo of context.payload.repositories_added) {
+    for (const repoData of context.payload.repositories_added) {
       handleNewRepo({
         context,
-        addedRepo,
+        repoData,
       });
     }
   });
-};
-
-const handleNewRepo = async ({ context, addedRepo }) => {
-  const [owner, repo] = addedRepo.full_name.split("/");
-
-  let existingReadme;
-  try {
-    existingReadme = await context.octokit.rest.repos.getContent({
-      owner,
-      repo,
-      path: "README.md",
-    });
-  } catch (error) {
-    if (error.status === 404) {
-      existingReadme = null;
-    } else {
-      console.error(`Error retrieving README.md for ${owner}/${repo}:`, error);
-    }
-  }
-
-  const generatedInviteLink = `https://repogate.vercel.app?owner=${owner}&repo=${repo}&installation_id=${context.payload.installation.id}`;
-
-  let newContent;
-  if (existingReadme) {
-    const existingContent = Buffer.from(existingReadme.data.content, "base64").toString();
-    newContent = `${existingContent}\n\n# [Get Collab Invite](${generatedInviteLink})`;
-  } else {
-    newContent = `# [Get Collab Invite](${generatedInviteLink})`;
-  }
-
-  const message = existingReadme ? "Update README.md" : "Create README.md";
-  const content = Buffer.from(newContent).toString("base64");
-
-  const committer = {
-    name: "token-gate-bot",
-    email: "repotokengate@gmail.com",
-  };
-
-  try {
-    await context.octokit.rest.repos.createOrUpdateFileContents({
-      owner,
-      repo,
-      path: "README.md",
-      message,
-      content,
-      committer,
-      sha: existingReadme ? existingReadme.data.sha : undefined,
-    });
-    console.log(`${existingReadme ? "Updated" : "Created"} readme for ${owner}/${repo}`);
-  } catch (error) {
-    console.error(`Error updating/creating README.md for ${owner}/${repo}:`, error);
-  }
 };
